86aa57ef3222c1876615a280d682de9a1ff0207a
İçimi Demirağ <74818535+icimidemirag@users.noreply.github.com>
2022-08-08 11:21:54 +0300

    Update Odev4-CRUD.js

--- Odev4-CRUD.js
+++ Odev4-CRUD.js
@@ -1,26 +1,30 @@
 const fs = require('fs');
 
-//File printing
-fs.writeFile('employees.json', '{"name": "Employee 1 Name", "salary":2000}', 'utf-8', (err) => {
-    if(err) console.log(err);
-    console.log('Dosya oluşturuldu.');
-})
+// //File printing
+// fs.writeFile('employees.json', '[{"name": "Employee 1 Name", "salary":2000}]', 'utf-8', (err) => {
+//     if(err) console.log(err);
+//     console.log('Dosya oluşturuldu.');
+// })
 
-//File reading
-fs.readFile('employees.json', 'utf-8', (err, data) => {
-    if(err) console.log(err);
-    console.log(data);
-    console.log('Dosya okundu.');
-})
+// //File reading
+// fs.readFile('employees.json', 'utf-8', (err, data) => {
+//     if(err) console.log(err);
+//     console.log(data);
+//     console.log('Dosya okundu.');
+// })
 
+const fileName = './employees.json';
+const file = require(fileName);
+file.push({"name": "Employee 2 Name", "salary": 3000} )
 //Data update
-fs.appendFile('employees.json', ',\n{"name":"Employees 2 Name", "salary":5000}', 'utf-8', (err) => {
-    if(err) console.log(err);
-    console.log('Dosya güncellendi.');
-})
+fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+    if (err) return console.log(err);
+    console.log(JSON.stringify(file));
+    console.log('writing to ' + fileName);
+});
 
-//File deleting
-fs.unlink('employees.json', (err) => {
-    if(err) console.log(err);
-    console.log('Dosya silindi.');
-})
+// //File deleting
+// fs.unlink('employees.json', (err) => {
+//     if(err) console.log(err);
+//     console.log('Dosya silindi.');
+// })
